072a21ebee7bb08a9a9cf3d53ac84acd6b1e22ed
vijayakumar191101-byte <vijayakumar191101@example.com>
2026-02-28 16:30:26 +0530

    slide title and description changes

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import slide1 from "../assets/slide1.jpg";
-import slide2 from "../assets/slide2.jpeg";
+import slide2 from "../assets/slide2.jpg";
 import slide3 from "../assets/slide3.jpg";
 
 const slides = [
